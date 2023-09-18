7aa8b07f52964d43c0f1c59fef425b23566b76e8
grace <kdjsanh0217@naver.com>
2023-09-18 11:13:58 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import './components/ProductReview/ProductReview.css';
+import ScrollToTop from './components/ScrollToTop';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<main>
+				<ScrollToTop />
+				<Outlet />
+			</main>
+		</>
+	);
+}
+
+export default App;
